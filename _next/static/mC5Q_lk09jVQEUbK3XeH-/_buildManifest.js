5c1d432d7dd61a295a823dc01e80166fc70210a3
sotta-tis <werfer.sou@gmail.com>
2024-12-23 01:16:38 +0900

    Updates

--- _next/static/mC5Q_lk09jVQEUbK3XeH-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-4ce747eecaf59cd4.js"],"/_error":["static/chunks/pages/_error-6b08de136861c9d2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
