b979c604fd1406a1bf53db54c8f507f4698d8291
sotta-tis <werfer.sou@gmail.com>
2024-12-22 22:08:25 +0900

    Updates

--- _next/static/l71PXpIWxVthm45Brexx-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-fd76420156f39d95.js"],"/_error":["static/chunks/pages/_error-6b08de136861c9d2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
