9d60b5031b7142cfa98d9df506db4a8fd9a62899
sotta-tis <werfer.sou@gmail.com>
2025-01-07 13:23:23 +0900

    :hammer: 謎処理消す

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,45 +1,23 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/router";
 import Header from "./Header";
 import Footer from "./Footer";
-
+import Head from "next/head";
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const [loading, setLoading] = useState(false);
-  const router = useRouter();
-
-  useEffect(() => {
-    const handleStart = () => setLoading(true);
-    const handleComplete = () => setLoading(false);
-
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
-
-    return () => {
-      router.events.off("routeChangeStart", handleStart);
-      router.events.off("routeChangeComplete", handleComplete);
-      router.events.off("routeChangeError", handleComplete);
-    };
-  }, [router]);
-
   return (
-    <div className="flex flex-col min-h-screen">
-      {loading && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-100 z-50">
-          <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent border-solid rounded-full animate-spin"></div>
-        </div>
-      )}
-
-      <Header />
-      <title>Sota Toshizumi Portfolio</title>
-      <meta name="description" content="Sota Toshziumi Portfolio" />
-      <main className="flex-grow">{children}</main>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>Sota Toshizumi Portfolio</title>
+        <meta name="description" content="Sota Toshziumi Portfolio" />
+      </Head>
+      <main className="flex-grow w-auto">
+        <Header />
+        {children}
+        <Footer />
+      </main>
+    </>
   );
 };
 
